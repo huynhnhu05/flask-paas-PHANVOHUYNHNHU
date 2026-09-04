636698cb4d48da4133a68b6554c58bab4af901d6
PHANVOHUYNHNHU <233404050199@ldxh.edu.vn>
2026-09-04 18:51:37 +0700

    feat: them 2 API

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
+import os
 from flask import Flask
 import datetime
 import platform
-
 app = Flask(__name__)
-
-
+visit_count = 0 
 @app.route("/")
 def home():
     return f"""
@@ -18,20 +17,17 @@ def home():
                 max-width: 640px;
                 margin: 60px auto;
             }}
-
             .box {{
                 background: #DEAEF1;
                 border-left: 5px solid #F14E79;
                 padding: 24px;
                 border-radius: 8px;
             }}
-
             h1 {{
                 color: #4F46E5;
             }}
         </style>
     </head>
-
     <body>
         <h1>Ứng dụng Flask trên PaaS - phiên bản 2</h1>
 
@@ -50,12 +46,24 @@ def home():
     </body>
     </html>
     """
-
-
 @app.route("/health")
 def health():
     return {"status": "ok"}
 
-
+@app.route("/api/counter")
+def counter():
+    global visit_count
+    visit_count += 1
+    return {
+        "so_lan_truy_cap": visit_count,
+        "ghi_chu": "So nay se MAT khi container khoi dong lai!"
+    }
+@app.route("/api/info")
+def info():
+    ten_sinh_vien = os.environ.get("STUDENT_NAME", "Chua dat bien moi truong")
+    return {
+        "sinh_vien": ten_sinh_vien,
+        "nguon_du_lieu": "Environment Variable tren Render, KHONG hardcode trong code"
+    }
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
